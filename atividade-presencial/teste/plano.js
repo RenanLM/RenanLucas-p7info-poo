c7e131391961e65d56776d232e8994ed304203af
RenanLucasdeMoura <99264208+RenanLucasdeMoura@users.noreply.github.com>
2022-05-26 21:09:19 -0300

    Delete atividade-presencial directory

--- atividade-presencial/teste/plano.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Quadrilater_1 = require("./Quadrilater");
-var teste = new Quadrilater_1.Ponto_AB(2, 3);
-teste.pertenceArea();
--- atividade-presencial/teste/plano.ts
+++
@@ -1,6 +0,0 @@
-import { Ponto_AB } from "./Quadrilater";
-import { Ponto } from "./Ponto";
-
-let teste = new Ponto(12, 13);
-
-teste.pertenceArea();
